cad6f3e3ebb3879f8810dd2f44164fdf342a984f
Ian Kottman <ikottman@users.noreply.github.com>
2020-05-23 00:45:55 -0500

    random moving rabbits

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import * as PIXI from "pixi.js";
 
 export const SCREEN_SIZE = window.innerHeight; // max height we can render in
-export const GRID_SIZE = 15; // number of rows/columns in our grid
+export const GRID_SIZE = 20; // number of rows/columns in our grid
 export const SPRITE_SIZE = SCREEN_SIZE / GRID_SIZE; // size each grid box should be
 export const APP = new PIXI.Application({
   width: SCREEN_SIZE, // make square
--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,61 @@ import rabbitImage from "./assets/rabbit.png";
 import Sprite from './models/sprite';
 import { APP, GRID_SIZE } from './constants';
 document.body.appendChild(APP.view);
+// all the sprites on the grid. An empty space is null
+const map: Array<Array<Sprite>> = Array(GRID_SIZE).fill(0).map(() => Array(GRID_SIZE).fill(null));
 
-// create row of bunnies for stress testing
-const bunnies: Array<Sprite> = [];
+function spaceOccupied(x: number, y: number) {
+    return map[x][y] != null;
+}
+
+function inBounds(x: number, y: number) {
+    return x > 0 && x < GRID_SIZE && y > 0 && y < GRID_SIZE;
+}
+
+function invalidMove(x?: number, y?: number) {
+    return x == null || x == undefined || 
+           y == null || y == undefined ||
+           !inBounds(x, y) ||
+           spaceOccupied(x, y);
+}
+
+// random x, y both in range [-1, 1]
+function randomMove() {
+    const x = Math.floor(Math.random() * 3) - 1;
+    const y = Math.floor(Math.random() * 3) - 1;
+    return [x, y];
+  }
+
+// fill first column with sprites
 for (let i = 0; i < GRID_SIZE; i++) {
-    bunnies.push(new Sprite(rabbitImage, 0, i));
+    map[0][i] = new Sprite(rabbitImage, 0, i);
 }
 
-// move back and forth
-let flip = false;
+// make random valid moves
 function moveBunny(bunny: Sprite) {
-    if (bunny.x == GRID_SIZE) {
-        flip = true;
-    } else if (bunny.x == 0) {
-        flip = false;
+    // brute force finding a valid move with max attempts
+    let x, y;
+    let tries = 0;
+    do {
+        let [xDelta, yDelta] = randomMove();
+        x = bunny.x + xDelta;
+        y = bunny.y + yDelta;
+        tries++;
     }
-    if (bunny.x > 0 && flip ) {
-        bunny.move(bunny.x - 1, bunny.y);
-    } else if (bunny.x < GRID_SIZE) {
-        bunny.move(bunny.x + 1, bunny.y);
+    while (invalidMove(x, y) && tries < 10);
+
+    if (!invalidMove(x, y)) {
+        bunny.move(x, y);
     }
 }
 
+const bunnies: Array<Sprite> = [].concat(...map).filter((s: Sprite | null) => s != null);
 // game clock
+let turn = 0;
 APP.ticker.add(() => {
     bunnies.forEach(bunny => moveBunny(bunny));
+    console.log(++turn);
+    if (turn > 1000) {
+        throw Error();
+    }
 });
