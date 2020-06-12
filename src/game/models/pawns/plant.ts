0da8b1834ffe74524bb063837500327cfa9aea3d
teofrastusb <teofrastusb@gmail.com>
2020-06-11 19:33:38 -0500

    Added Plant Atributes

--- src/game/models/pawns/plant.ts
+++ src/game/models/pawns/plant.ts
@@ -6,10 +6,20 @@ export class Plant implements Pawn {
   type = PAWN_TYPE.PLANT;
   x: number;
   y: number;
+  maxHp: number;
+  hp: number;
+  xp: number;
+  lvl: number;
+  canSeed: number;
 
   constructor(x: number, y: number) {
     this.id = PIXI.utils.uid();
     this.x = x;
     this.y = y;
+    this.maxHp = 10;
+    this.hp = 10;
+    this.xp = 0;
+    this.lvl = 1;
+    this.canSeed = 0;
   }
 }
