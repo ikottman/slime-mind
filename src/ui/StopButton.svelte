03048ddff45f51d998e46d863ac725fb84dee617
Ian Kottman <ikottman@users.noreply.github.com>
2020-07-18 19:43:09 -0500

    use shoelace buttons

--- src/ui/StopButton.svelte
+++ src/ui/StopButton.svelte
@@ -1,31 +1,12 @@
 <script>
   import { APP } from './store';
-  
+
   function handleClick() {
     APP.ticker.stop();
   }
 </script>
 
-<style>
-  button {
-    display: inline-block;
-    padding: 0.3em 1.2em;
-    margin: 0 0.1em 0.1em 0;
-    border: 0.16em solid rgba(255,255,255,0);
-    border-radius: 2em;
-    font-weight: 300;
-    font-size: .6em;
-    text-shadow: 0 0.04em 0.04em grey;
-    text-align: center;
-    transition: all 0.2s;
-    background-color: #E1341E;
-  }
-
-  button:hover{
-   border-color: black;
-  }
-</style>
-
-<button on:click={handleClick}>
-Pause
-</button>
+<sl-button type='danger' size='small' on:click={handleClick}>
+  <sl-icon slot="prefix" name="stop"></sl-icon>
+  Pause
+</sl-button>
