26411a912f6b1a650de09ec65332acad9043485d
Ian Kottman <ikottman@users.noreply.github.com>
2020-07-25 17:06:32 -0500

    improve eat n seek pathfinding

--- src/game/ai/ArchiveOfGreatnesss/EatNSeek.ts
+++ src/game/ai/ArchiveOfGreatnesss/EatNSeek.ts
@@ -25,6 +25,80 @@ export class EatNSeek {
     return {x, y};
   }
 
+  Node(point, distance, previous) {
+    const id = this.nodeId(point);
+    const {x, y} = point;
+    const pawn = point.type ? point : undefined;
+    return {id, x, y, pawn, distance, previous};
+  }
+
+  flattenMap() {
+    const flat = [];
+    for (let i = 0; i < this.gameMap.length; i++) {
+      for (let j = 0; j < this.gameMap.length; j++) {
+        const current = this.gameMap[i][j];
+        flat.push(current ? current : this.pt(i, j));
+      }
+    }
+    return flat;
+  }
+
+  nodeId(point) {
+    return `${point.x},${point.y}`;
+  }
+
+  pathToTarget(source, target) {
+    const nodes = [];
+    const nodesMap = {}; // use a map so it's constant time to find a node
+    this.flattenMap().forEach(cell => {
+      const node = cell.x === source.x && cell.y === source.y ? this.Node(cell, 0, undefined) : this.Node(cell, Infinity, undefined);
+      nodesMap[node.id] = node;
+      nodes.push(node)
+    });
+    let current;
+
+    const targetId = this.nodeId(target);
+    const sourceId = this.nodeId(source);
+    // until we've seen all cells
+    while (nodes.length > 0) {
+      // find closest one (starts with source)
+      nodes.sort((a, b) => a.distance - b.distance);
+      current = nodes.shift();
+
+      // once we find the target we know we have at least one minimal path
+      if (current.id === targetId) {
+        break;
+      }
+
+      // figure out how far each neighbor is from the source
+      this.neighborCells(current)
+        .forEach(neighbor => {
+          const neighborNode = nodesMap[this.nodeId(neighbor)];
+          if (!neighborNode) {
+            // skip nodes we've seen before
+            return;
+          }
+
+          let alt = current.distance + 1; // all neighbors are 1 away
+          // try to avoid high hp occupants
+          if (current.pawn) {
+            alt = alt + current.pawn.hp;
+          }
+          if (alt < neighborNode.distance) {
+            neighborNode.distance = alt;
+            neighborNode.previous = current;
+          }
+        });
+    }
+
+    // walk backwards from target until we find the first step in the shortest path
+    while (current.previous.id !== sourceId) {
+      current = current.previous
+    }
+
+    return current;
+  }
+
   // [min, max]
   randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -46,11 +120,16 @@ export class EatNSeek {
     return Math.abs(target.x - source.x) + Math.abs(target.y - source.y);
   }
 
+  // return point or pawn that is one cardinal direction away from the given point
   neighbors(x, y) {
+    // return either the pawn or coords to an empty cell
+    return this.neighborCells(this.pt(x, y)).map(cell => this.gameMap[cell.x][cell.y] || cell);
+  }
+
+  neighborCells(point) {
     return [this.pt(-1, 0), this.pt(0, -1), this.pt(0, 1), this.pt(1, 0)]
-      .map(cell => this.pt(x + cell.x, y + cell.y))
-      .filter(cell => this.gameMap[cell.x] !== undefined && this.gameMap[cell.x][cell.y] !== undefined) // remove out of bounds
-      .map(cell => this.gameMap[cell.x][cell.y] || cell); // return either the pawn or coords to an empty cell
+      .map(cell => this.pt(point.x + cell.x, point.y + cell.y))
+      .filter(cell => this.gameMap[cell.x] !== undefined && this.gameMap[cell.x][cell.y] !== undefined); // remove out of bounds
   }
 
   setState(gameMap, id, configuration, turn) {
@@ -68,20 +147,21 @@ export class EatNSeek {
     this.nearbyPlants = neighbors.filter(p => p.type === 'PLANT');
     this.nearbyBoids = neighbors.filter(p => p.type === 'SLIME' && p.owner === this.playerId);
     this.nearbyEnemies = neighbors.filter(p => p.type === 'SLIME' && p.owner !== this.playerId);
+    this.findTarget();
 
     // possible moves
     this.nearbyCells = neighbors.filter(cell => cell.type === undefined);
-
-    this.findTarget();
   }
 
   eating() {
-    return this.flock.every(s => s.level < 8);
+    return this.flock.every(s => s.level < 8) && this.plants.length;
   }
 
   findEnemy() {
     // find enemy with highest xp
-    return this.enemies.sort((a, b) => b.xp - a.xp)[0];
+    //return this.enemies.sort((a, b) => b.xp - a.xp)[0];
+    // find enemy with lowest hp
+    return this.enemies.sort((a, b) => a.hp - b.hp)[0];
   }
 
   findPlant() {
@@ -125,23 +205,23 @@ export class EatNSeek {
     };
   }
 
-  shouldSplit() {
-    const desiredFlockSize = 6;
-    return this.boid.level >= this.configuration.slime.minSplitLevel && this.nearbyCells.length && this.flock.length < desiredFlockSize;
-  }
-
   closestTo(options, target) {
     return options.sort((a, b) => this.distance(a, target) - this.distance(b, target))[0];
   }
 
   move() {
-    const {x, y} = this.closestTo(this.nearbyCells, this.target);
+    const path = this.pathToTarget(this.boid, this.target);
 
-    return {
-      action: 'MOVE',
-      x,
-      y
+    if (path && !path.pawn) {
+      return {
+        action: 'MOVE',
+        x: path.x,
+        y: path.y
+      }
     }
+    return {
+      action: 'NOTHING'
+    };
   }
 
   debug() {
@@ -162,21 +242,11 @@ export class EatNSeek {
     }
 
     // level up
-    if (this.eating()) {
-      if (this.nearbyPlants.length) {
-        return this.eat();
-      }
-      if (this.nearbyCells.length) {
-        return this.move();
-      }
-    }
-
-    // seek and destroy
-    if (this.nearbyCells.length) {
-      return this.move();
-    }
     if (this.nearbyPlants.length) {
       return this.eat();
+    }
+    if (this.nearbyCells.length) {
+      return this.move();
     }
 
     return { action: 'NOTHING'};
