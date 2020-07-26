db04c212e25ebb99488e158b092450fd4c6a02ee
Ian Kottman <ikottman@users.noreply.github.com>
2020-07-26 16:50:42 -0500

    fix multiple bugs in pathfinding

--- src/game/ai/ArchiveOfGreatnesss/EatNSeek.ts
+++ src/game/ai/ArchiveOfGreatnesss/EatNSeek.ts
@@ -26,11 +26,11 @@ export class EatNSeek {
     return {x, y};
   }
 
-  Node(point, distance, previous) {
+  Node(point, distance) {
     const id = this.nodeId(point);
     const {x, y} = point;
     const pawn = point.type ? point : undefined;
-    return {id, x, y, pawn, distance, previous};
+    return {id, x, y, pawn, distance};
   }
 
   flattenMap() {
@@ -51,20 +51,17 @@ export class EatNSeek {
   // returns point to move to on the shortest path to target
   // or null if it exceeds the max search depth
   pathToTarget(source, target) {
-    const nodes = [];
-    const nodesMap = {}; // use a map so it's constant time to find a node
-    this.flattenMap().forEach(cell => {
-      const node = cell.x === source.x && cell.y === source.y ? this.Node(cell, 0, undefined) : this.Node(cell, Infinity, undefined);
-      nodesMap[node.id] = node;
-      nodes.push(node)
-    });
+    const nodes = this.flattenMap()
+    .filter(cell => cell.owner !== this.playerId && cell.type !== 'ROCK')
+    .map(cell => this.Node(cell, Infinity));
+    nodes.unshift(this.Node(source, 0)); // a distance of 0 guarantees we'll start the search from the source
     let current;
 
     const targetId = this.nodeId(target);
-    const sourceId = this.nodeId(source);
+    const sourceId = nodes[0].id;
     // until we've seen all cells
     while (nodes.length > 0) {
-      // find closest one (starts with source)
+      // find closest one
       nodes.sort((a, b) => a.distance - b.distance);
       current = nodes.shift();
 
@@ -75,7 +72,7 @@ export class EatNSeek {
 
       // figure out how far each neighbor is from the source
       this.neighborCells(current).forEach(neighbor => {
-        const neighborNode = nodesMap[this.nodeId(neighbor)];
+        const neighborNode = nodes.find(n => n.id === this.nodeId(neighbor));
         if (!neighborNode) {
           // skip nodes we've seen before
           return;
@@ -83,14 +80,7 @@ export class EatNSeek {
 
         let cost = current.distance + 1; // all neighbors are 1 away
         cost = cost + this.distance(neighbor, target); // penalize looking at nodes that are far from the target
-        if (neighborNode.pawn) {
-          // can never walk through rocks or myself
-          if (neighborNode.pawn.type === 'ROCK' || neighborNode.pawn.owner === this.playerId) {
-            cost = Infinity;
-          } else { // penalize trying to go through trees so we prefer empty cell
-            cost = cost + neighborNode.pawn.hp;
-          }
-        }
+        cost = neighbor.pawn ? cost + neighbor.pawn.hp : cost; // penalize occupied cells
         if (cost < neighborNode.distance) {
           neighborNode.distance = cost;
           neighborNode.previous = current;
@@ -101,9 +91,6 @@ export class EatNSeek {
     // walk backwards from target until we find the first step in the shortest path
     while (current.previous !== undefined && current.previous.id !== sourceId) {
       current = current.previous
-    }
-    if (current.previous === undefined) { // TODO: how can this happen???
-      return null;
     }
     return current;
   }
@@ -219,15 +206,14 @@ export class EatNSeek {
   }
 
   move() {
-    // default to naive pathfinding if the pathToTarget fails
+    // default to naive pathfinding if the pathToTarget fails or its too far
     let {x, y} = this.closestTo(this.nearbyCells, this.target);
-
-    const path = this.pathToTarget(this.boid, this.target);
-    if (path && !path.pawn) {
-      x = path.x;
-      y = path.y;
-    } else {
-      console.log('tried to move into pawn');
+    if (this.nearbyCells.length > 1 && this.distance(this.boid, this.target) < 15) {
+      const path = this.pathToTarget(this.boid, this.target);
+      if (path && !path.pawn) {
+        x = path.x;
+        y = path.y;
+      }
     }
 
     return {
